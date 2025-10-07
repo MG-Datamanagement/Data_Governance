56f9a21dbaa6eebcd27df8815373f2dd08e0251e
arunchary <rp.ran@ncs.co>
2025-10-07 17:56:58 +0530

    util fn for normalization for lineage data to mitigate repsonse changes of id,name of table

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,15 @@
+import { LineageData } from "../components/LineageGraph/types";
+
+export const normalizeLineageData = (apiData: any): LineageData => {
+  return {
+    center_table: {
+      table_id: apiData.center_table.id,           // Map id to table_id - with recent changes
+      table_name: apiData.center_table.name,       // Map name to table_name - with recent changes
+      schema_name: apiData.center_table.schema_name,
+      data_source_name: apiData.center_table.data_source_name
+    },
+    upstream_links: apiData.upstream_links || [],
+    downstream_links: apiData.downstream_links || [],
+    metadata: apiData.metadata
+  };
+};
